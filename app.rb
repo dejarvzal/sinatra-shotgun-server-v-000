3884975b0add6494a9799af7afda68ddc0178fc9
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2020-04-19 06:06:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Started my server using Shotgun!"
+    "Started my server using Shotgun!
+    Welcome to my App!!! I BUILT THIS! "
   end
 
 end
